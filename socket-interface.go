5c0be692555a1d3b2b28fe6b09a014c4a69f5b04
fritz <franciscosantin2002@gmail.com>
2024-01-31 14:54:12 -0300

    método Emit agregado

--- socket-interface.go
+++ socket-interface.go
@@ -9,8 +9,8 @@ import (
 type Server struct {
 	listener net.Listener
 	sockets  []net.Conn
-	rooms    map[string]net.Conn
-	events   map[string]func()
+	rooms    map[string][]net.Conn
+	events   map[string]func(data string, socket net.Conn)
 }
 
 func (srv *Server) Init(address string) {
@@ -20,6 +20,7 @@ func (srv *Server) Init(address string) {
 	if err != nil {
 		log.Fatal("Couldn't start")
 	}
+	srv.addDefaultEventListeners()
 }
 
 func (srv *Server) WaitForClients() {
@@ -51,11 +52,52 @@ func (srv Server) parseMessage(msg []byte, socket net.Conn) {
 	stringMsg := string(msg)
 	for eventName := range srv.events {
 		if strings.Contains(stringMsg, eventName) {
-			srv.events[eventName]()
+			srv.events[eventName](stringMsg[len(eventName):], socket)
 		}
 	}
 }
 
-func (srv Server) AddEventListener(event string, handler func()) {
+func (srv Server) AddEventListener(event string, handler func(data string, socket net.Conn)) {
 	srv.events[event] = handler
+}
+
+func (srv Server) addDefaultEventListeners() {
+	srv.AddEventListener("join", func(roomName string, socket net.Conn) {
+		srv.rooms[roomName] = append(srv.rooms[roomName], socket)
+	})
+}
+
+type EmissionParams struct {
+	room   string
+	socket net.Conn
+	event  string
+	data   string
+}
+
+func (srv Server) Emit(params EmissionParams) {
+	if params.room != "" {
+		srv.emitToRoom(params.room, params.event, params.data)
+	} else if params.socket != nil {
+		srv.emitToSocket(params.socket, params.event, params.data)
+	} else {
+		srv.emitToAllSockets(params.event, params.data)
+	}
+}
+
+func (srv Server) emitToRoom(room string, event string, data string) {
+	sockets := srv.rooms[room]
+	for i := range sockets {
+		srv.emitToSocket(sockets[i], event, data)
+	}
+}
+
+func (srv Server) emitToAllSockets(event string, data string) {
+	for i := range srv.sockets {
+		srv.emitToSocket(srv.sockets[i], event, data)
+	}
+}
+
+func (srv Server) emitToSocket(socket net.Conn, event string, data string) {
+	finalMessage := event + data
+	socket.Write([]byte(finalMessage))
 }
